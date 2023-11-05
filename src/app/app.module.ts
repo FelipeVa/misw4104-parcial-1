00b1b3683e6b78a3148ac5e9e5ef9a9beae0f737
Felipe Valencia <pipeedev@gmail.com>
2023-11-05 16:12:35 -0500

    fix: fixing tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CafesModule } from './cafes/cafes.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ListarCafesComponent } from "./cafes/listar-cafes/listar-cafes.component";
 
 @NgModule({
   declarations: [AppComponent],
